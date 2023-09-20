34ba4e7ddb25c1e8ea913b375001ae5b71ce922d
Bakoulis George <g.bakoulis@outlook.com>
2023-09-20 19:43:09 +0300

    used the .env package

--- config.php
+++ config.php
@@ -1,12 +1,17 @@
 <?php
 
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/.env');
+
 return [
     'database' => [
-        'host' => 'localhost',
-        'dbname' => 'web_speed_reports',
-        'port' => '3306',
+        'host' => $_ENV['DB_HOST'],
+        'dbname' => $_ENV['DB_NAME'],
+        'port' => $_ENV['DB_PORT'],
         'charset' => 'utf8mb4'
     ],
-    'username' => 'root',
-    'pass' => ''
+    'username' => $_ENV['DB_USER'],
+    'pass' => $_ENV['DB_PASS']
 ];
